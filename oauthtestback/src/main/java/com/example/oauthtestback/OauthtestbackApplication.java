27c5477d529bf98eaa72fcfa8b724734e4b35ea1
JangHongJoon <wkdghdwns19969@gmail.com>
2023-10-04 22:29:56 +0900

    feat : Created Spring Project

---
+++ oauthtestback/src/main/java/com/example/oauthtestback/OauthtestbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthtestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthtestbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthtestbackApplication.class, args);
+	}
+
+}
---
+++ oauthtestback/src/test/java/com/example/oauthtestback/OauthtestbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthtestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthtestbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
