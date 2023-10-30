82954f264d2c4fc139905e712c4cd331b3c69241
ash <hyeonsian23@gmail.com>
2023-10-30 19:50:51 +0900

    feat : 비밀번호 초기화 페이지 생성

--- oauthtestfront/src/App.js
+++ oauthtestfront/src/App.js
@@ -2,17 +2,23 @@ import React from "react";
 import "./App.css";
 import LoginPage from "./component/login/LoginPage";
 import JoinPage from "./component/join/JoinPage";
+import FindPw from "./component/find/FindPw";
 import { Routes, Route, Link } from "react-router-dom";
 import LoginHandler from "./component/login/LoginHandler";
 
 function App() {
   return (
     <div className="App">
-      <Link to="/login">로그인화면</Link>
-      <Link to="/join">회원가입화면</Link>
+      <button>
+      <Link to="/login">로그인화면</Link></button>
+      <button>
+      <Link to="/join">회원가입화면</Link></button>
+      <button>
+      <Link to="/findpw">비밀번호 초기화</Link></button>
       <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/join" element={<JoinPage />}></Route>
+        <Route path="/findpw" element={<FindPw />}></Route>
         <Route path="/user/kakao/callback" element={<LoginHandler />}></Route>
       </Routes>
     </div>
---
+++ oauthtestfront/src/component/find/FindPw.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Header from "../background/Header";
+import { Container, Input, PasswordInput, Btn } from "../login/LoginPage";
+
+// styled-componets
+
+
+function FindPw() {
+  return (
+    <>
+      <Container>
+        <Header title="비밀번호 초기화" />
+        <div>
+          <Input type="id" placeholder="비밀번호" />
+        </div>
+        <div>
+            <Btn>찾기</Btn>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default FindPw;
