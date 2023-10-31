47aec5b92d7beaf2a7474763e2023684d6ead7fb
ash <hyeonsian23@gmail.com>
2023-11-01 02:17:15 +0900

    feat : 로고 이미지 클릭하면 메인으로 이동하도록 수정

--- oauthtestfront/src/component/background/Header.js
+++ oauthtestfront/src/component/background/Header.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import {Head} from "../../styles/background/Head";
-
+import { Head } from "../../styles/background/Head";
 
 function Header(props) {
   return (
     <Head>
-      
-      <img
-        src={process.env.PUBLIC_URL + "/logo.png"}
-        alt="logo"
-        width="80px"
-        height="auto"
-      />
-      <h2 style={{ color:'#efefef', letterSpacing: '2px'}}>{props.title}</h2>
+      <Link to="/">
+        <img
+          src={process.env.PUBLIC_URL + "/logo.png"}
+          alt="logo"
+          width="80px"
+          height="auto"
+        />
+      </Link>
+      <h2 style={{ color: "#efefef", letterSpacing: "2px" }}>{props.title}</h2>
     </Head>
   );
 }
