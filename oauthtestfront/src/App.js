1a69edb887a56591139a72d496f4f82f23851298
ash <hyeonsian23@gmail.com>
2023-11-01 14:01:39 +0900

    feat : 로그아웃 기능 추가 (로그인 기능 테스트용)

--- oauthtestfront/src/App.js
+++ oauthtestfront/src/App.js
@@ -5,18 +5,38 @@ import JoinPage from "./component/join/JoinPage";
 import FindPwPage from "./component/find/FindPwPage";
 import { Routes, Route, Link } from "react-router-dom";
 import LoginHandler from "./component/login/LoginHandler";
+import supabase from "./component/supabase";
 
 function App() {
-  document.title = '툰더';
+  document.title = "툰더";
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.error("로그아웃 중 오류가 발생했습니다.", error);
+    } else {
+      // 로그아웃 성공한 경우 로그아웃 메시지를 표시하거나 원하는 작업을 수행할 수 있습니다.
+      console.log("로그아웃되었습니다.");
+    }
+  };
 
   return (
     <div className="App">
       <button>
-      <Link to="/login">로그인</Link></button>
+        <Link to="/login">로그인</Link>
+      </button>
       <button>
-      <Link to="/join">회원가입</Link></button>
+        <Link to="/" onClick={handleLogout}>
+          로그아웃
+        </Link>
+      </button>
       <button>
-      <Link to="/findpw">비밀번호 초기화</Link></button>
+        <Link to="/join">회원가입</Link>
+      </button>
+      <button>
+        <Link to="/findpw">비밀번호 초기화</Link>
+      </button>
       <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/join" element={<JoinPage />}></Route>
