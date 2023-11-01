fe9cf41ca33239e7236897d79261c22cb6ff31a4
ash <hyeonsian23@gmail.com>
2023-11-02 01:27:53 +0900

    feat : 회원가입 페이지에서 user 정보 제대로 입력했을 때 버튼 색이 변하도록 수정

---
+++ oauthtestfront/src/component/join/JoinPageStep1.js
@@ -0,0 +1,11 @@
+          <Btn
+            style={{
+              marginBottom: "50px",
+              // input들이 정확히 입력되지 않았을 때는 버튼 색이 원래의 50%만큼 투명하게 보이도록 설정
+              backgroundColor: notAllow ? "#6e6e6e88" : "#6e6e6e",
+            }}
+            type="submit"
+            disabled={notAllow}
+          >
+            다음
+          </Btn>
