a935e98bf1d5c8c6561b6d608eec24983ff0721a
ash <hyeonsian23@gmail.com>
2023-11-02 01:35:43 +0900

    refactor : 버튼 활성화시 색상 대비 효과 좀 더 강하게 수정

--- oauthtestfront/src/component/join/JoinPageStep2.js
+++ oauthtestfront/src/component/join/JoinPageStep2.js
@@ -88,7 +88,7 @@ function JoinPageStep2() {
         <Btn
           style={{
             // input들이 정확히 입력되지 않았을 때는 버튼 색이 원래의 50%만큼 투명하게 보이도록 설정
-            backgroundColor: notAllow ? "#6e6e6e88" : "#6e6e6e",
+            backgroundColor: notAllow ? "#6e6e6e66" : "#6e6e6e",
           }}
           type="submit"
           onClick={handleSubmit}
